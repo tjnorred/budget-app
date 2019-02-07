519d89bdd98bb128c5c99145a6c874644069af1a
TJ <tjnorred@gmail.com>
2019-02-07 17:40:55 -0500

    fix issue #6

--- js/app.js
+++ js/app.js
@@ -122,10 +122,10 @@ editExpense(element){
   let parent = element.parentElement.parentElement.parentElement;
   //remove from DOM
   this.expenseList.removeChild(parent);
-  //remove from DOM
   let expense = this.itemList.filter(function(item){
     return item.id === id;
   })
+  // show value
   this.expenseInput.value = expense[0].title;
   this.amountInput.value = expense[0].amount;
   //remove from the list
